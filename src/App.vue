4acc356077ead3860af2b42445443cf84d16f37d
Maya Marr <mmarr2@student.tgm.ac.at>
2026-01-08 17:38:44 +0100

    Buttons in App.vue added

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,40 @@
       <v-app-bar-title>Vue Data Table</v-app-bar-title>
       <v-spacer></v-spacer>
       <v-switch
-          v-model="isDark"
-          hide-details
-          inset
-          :label="isDark ? 'Dark' : 'Light'"
-          class="ma-2"
+        v-model="isDark"
+        hide-details
+        inset
+        :label="isDark ? 'Dark' : 'Light'"
+        class="ma-2"
       ></v-switch>
     </v-app-bar>
+
     <v-main>
-      <Button />
-      <DataTable />
+      <!-- Buttons for different tables -->
+      <v-row class="pa-4" dense>
+        <v-btn color="secondary" class="ma-2" @click="currentTable = 'analysis'">
+          Analysis
+        </v-btn>
+        <v-btn color="secondary" class="ma-2" @click="currentTable = 'box'">
+          Box
+        </v-btn>
+        <v-btn color="secondary" class="ma-2" @click="currentTable = 'boxpo'">
+          Boxpo
+        </v-btn>
+        <v-btn color="secondary" class="ma-2" @click="currentTable = 'log'">
+          Log
+        </v-btn>
+        <v-btn color="secondary" class="ma-2" @click="currentTable = 'sample'">
+          Sample
+        </v-btn>
+        <v-btn color="secondary" class="ma-2" @click="currentTable = 'threshold'">
+          Threshold
+        </v-btn>
+
+      </v-row>
+
+      <!-- Render the selected table -->
+      <DataTable :table="currentTable" />
     </v-main>
   </v-app>
 </template>
@@ -29,6 +53,7 @@ export default {
   setup() {
     const theme = useTheme()
     const isDark = ref(false)
+    const currentTable = ref('table1')
 
     watch(isDark, (newValue) => {
       theme.global.name.value = newValue ? 'dark' : 'light'
@@ -36,6 +61,7 @@ export default {
 
     return {
       isDark,
+      currentTable,
     }
   },
 }
